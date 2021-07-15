bf7381932cb4ccb5b0b0737931bd95b7374a3025
Yichen Xu <yichen.x@outlook.com>
2021-07-15 17:26:33 +0800

    Initial Commit

---
+++ hello-macos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello-macos
+//
+//  Created by Yichen Xu on 2021/7/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello-macos/hello_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_macosApp.swift
+//  hello-macos
+//
+//  Created by Yichen Xu on 2021/7/15.
+//
+
+import SwiftUI
+
+@main
+struct hello_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
